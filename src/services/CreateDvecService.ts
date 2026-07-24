f5c4e5f11aecf10745c387632634a5c2dd6aa9e0
jonatasem <jonatas.em20@gmail.com>
2026-07-24 06:53:15 -0300

    feat: create service CreateDvecService

--- src/services/CreateDvecService.ts
+++ src/services/CreateDvecService.ts
@@ -10,14 +10,25 @@ export class CreateDvecService {
     async execute({ frota, atividade, qth }: CreateDvecProps){
 
         // Verificações
-        // 1º Verificar a existencia da frota
         if(!frota){
             throw new Error("Por favor, digite a frota do equipamento.");
         }
 
         if(!atividade){
-
+            throw new Error("Por favor, digite a atividade do equipamento.");
         }
+
+        if(!qth){
+            throw new Error("Por favor, digite o local do equipamento.");
+        }        
+
+        // Verificar a existencia da frota
+
+        if(frota){
+            throw new Error("Essa frota já foi cadastrada.");
+        }
+
+        
 
     }
 }
